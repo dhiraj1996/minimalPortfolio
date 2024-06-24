a44c53f029b2261fdab43ea47878aa510c68bca5
dhiraj1996 <dhirajkumar1996@gmail.com>
2024-06-25 01:46:22 +0530

    adding project cards and images

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import styles from "../sections/Projects/ProjectsStyles.module.css";
 
-function ProjectCard({src, link, h3, p}) {
+function ProjectCard({ src, link, h3, p, label}) {
+  // let [label] = label;
+  // const label = ["HTML","CSS","JAVASCRIPT", "FETCH", "API"];
   return (
     <>
-        <a 
-        href={link} 
-        target='_blank'>
-            <img src={src}
-                alt={`${h3} logo`}
-            />
-            <h3>{h3}</h3>
-            <p>{p}</p>
+      <div className={styles.cardContainer}>
+        <a href={link} target="_blank">
+          <img src={src} alt={`${h3} logo`} />
+          <h3>{h3}</h3>
+          <p>{p}</p>
+          {/* <span className="label label-default">{label}</span> */}
+          {label.map((lab, index) => (<span key={index} className="label label-default">{lab}</span>))}
         </a>
-
+      </div>
     </>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,11 +1,11 @@
 import styles from "./ProjectsStyles.module.css";
-import viber from "../../assets/viberr.png";
-import myntraClone from "../../../public/myntraProject.png"
+// import viber from "../../assets/viberr.png";
+import newsPic from "/jsnews.png"
+import myntraClone from "/myntraProject.png"
 // import myntraClone from "../../../public/myntraProject2.png"
-import SNS from "../../../public/Post-it-Home.png"
+import SNS from "/Post-it-Home.png"
+import ERP from "/erp.png"
 
-import hipsster from "../../assets/hipsster.png"
-import fitLift from "../../assets/fitlift.png"
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -14,28 +14,35 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viber}
+          src={newsPic}
           link="https://github.com/dhiraj1996/backendChai"
-          h3="News"
+          h3="Live News"
           p="News App"
+          label={["HTML","CSS","JAVASCRIPT","FETCH","NEWS-API"]}
         />
         <ProjectCard
           src={myntraClone}
           link="https://github.com/dhiraj1996/backendChai"
           h3="Myntra Clone"
           p="Shopping App"
+          label={["REACT","REDUX","HTML","CSS","BOOTSTRAP","JAVASCRIPT","FETCH","API"]}
+
         />
         <ProjectCard
           src={SNS}
           link="https://github.com/dhiraj1996/backendChai"
           h3="Post-IT"
           p="Social Media App"
+          label={["REACT","CONTEXT-API", "BOOTSTRAP","HTML","CSS","JAVASCRIPT","FETCH","API"]}
+
         />
         <ProjectCard
-          src={fitLift}
-          link="https://github.com/dhiraj1996/backendChai"
-          h3="FitLift"
-          p="Fitness App"
+          src={ERP}
+          link=" https://dhiraj1996.github.io/erpmain/"
+          h3="ERP for College"
+          p="Enterprise Resource Planner"
+          label={["WORDPRESS","HTML","CSS","JAVASCRIPT","PHP", "MYSQL","CRUD"]}
+
         />
       </div>
     </section>
